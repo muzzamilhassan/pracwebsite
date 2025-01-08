98f9a39bac6797a3ac67c69da687c6f9f1a5315c
Muzzamil hassan <129590453+muzzamilhassan@users.noreply.github.com>
2025-01-09 02:49:10 +0500

    Update page.js
    update

--- app/about/page.js
+++ app/about/page.js
@@ -9,7 +9,7 @@ const About = () => {
     <section className="about-us">
       <div className="row">
         <div className="about-col">
-          <h1>We are the world's largest University</h1>
+          <h1>We are the world{"'"}s largest University</h1>
           <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Inventore cumque in accusantium molestias maiores fuga vitae eos, ducimus officiis repudiandae voluptatem error. Laboriosam, numquam blanditiis aspernatur, nobis tempora consequatur id aliquam asperiores voluptatum iste animi voluptatem fugiat totam excepturi dolor.</p>
          <Button text="Explore data"/>
         </div>
